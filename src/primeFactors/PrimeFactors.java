64cef2f23554d19b82626125be1ff50c405778d9
youme <youme.bae@gmail.com>
2019-05-28 13:49:19 +0900

    Update PrimeFactors.java

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -1,5 +1,14 @@
 package primeFactors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
+
+	public List<Integer> of(int i) {
+		List<Integer> factors = new ArrayList<Integer>();
+		if(i==2)factors.add(2);
+		return factors;
+	}
 
 }
