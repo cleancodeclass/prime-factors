50875611e654fbddbba6ffb2c84fc69c0c1249d5
youme <youme.bae@gmail.com>
2019-05-28 14:17:26 +0900

    Update PrimeFactorsTest.java

--- test/primeFactors/PrimeFactorsTest.java
+++ test/primeFactors/PrimeFactorsTest.java
@@ -11,30 +11,63 @@ import org.junit.Test;
 public class PrimeFactorsTest {
 
 	private PrimeFactors primeFactors;
+
 	@Before
 	public void setUp() {
 		primeFactors = new PrimeFactors();
-		
+
 	}
 
 	@Test
 	public void primeFactorTestOf1() {
-		
-		//when    어떤 동작을 했을때
+
+		// when 어떤 동작을 했을때
 		List<Integer> factors = primeFactors.of(1);
-		
-		//then    어떤값이 나오는지 확인
+
+		// then 어떤값이 나오는지 확인
 		assertEquals(Arrays.asList(), factors);
+	}
+
+	@Test
+	public void primeFactorTestOf2() {
+
+		// when
+		List<Integer> factors = primeFactors.of(2);
+
+		// then
+		assertEquals(Arrays.asList(2), factors);
 	}
 	
 	@Test
-	public void primeFactorTestOf2() {
+	public void primeFactorTestOf3() {
 		
-		//when
-		List<Integer> factors = primeFactors.of(2);
-		
-		//then
-		assertEquals(Arrays.asList(2), factors);
+		// when
+		List<Integer> factors = primeFactors.of(3);
+
+		// then
+		assertEquals(Arrays.asList(3), factors);
 	}
+	
+	@Test
+	public void primeFactorTestOf4() {
+		
+		// when
+		List<Integer> factors = primeFactors.of(4);
+
+		// then
+		assertEquals(Arrays.asList(2,2), factors);
+	}
+	
+	@Test
+	public void primeFactorTestOf6() {
+		
+		// when
+		List<Integer> factors = primeFactors.of(6);
+
+		// then
+		assertEquals(Arrays.asList(2,3), factors);
+	}
+	
+	
 
 }
