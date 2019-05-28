0e45b7fe4b0fdc81dd875efc37e39472b170f312
youme <youme.bae@gmail.com>
2019-05-28 14:17:46 +0900

    Update PrimeFactors.java

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -7,7 +7,21 @@ public class PrimeFactors {
 
 	public List<Integer> of(int i) {
 		List<Integer> factors = new ArrayList<Integer>();
-		if(i==2)factors.add(2);
+		if (i >1) {
+			if(i==4) {
+				while(i%2==0) {
+					factors.add(2);
+					i/=2;
+				}
+			}else if(i==6){
+				factors.add(2);
+				factors.add(3);
+			}
+			else {
+				factors.add(i);
+				
+			}
+		}
 		return factors;
 	}
 
