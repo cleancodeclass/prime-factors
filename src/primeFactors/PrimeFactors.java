478527dec9482691a5d3c65d5049b6e29479f860
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 23:35:03 +0900

    Green-primefactor_of_9

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -12,6 +12,10 @@ public class PrimeFactors {
 				factors.add(2);
 				number /= 2;
 			}
+			while(number % 3 ==0) {
+				factors.add(3);
+				number /= 3;
+			}
 			if(number >1) {
 				factors.add(number);
 			}
