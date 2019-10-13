f2fc63a4f4317fc3583c8ee8f3400eb6b5e4554f
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 23:37:30 +0900

    Refactor-code re-arrange

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -7,8 +7,8 @@ public class PrimeFactors {
 
 	public List<Integer> of(int number) {
 		List<Integer> factors = new ArrayList<Integer>();
+		int divisor = 2;
 		if(number>1) {
-			int divisor = 2;
 			while(number % divisor == 0) {
 				factors.add(divisor);
 				number /= divisor;
@@ -19,9 +19,9 @@ public class PrimeFactors {
 				factors.add(divisor);
 				number /= divisor;
 			}
-			if(number >1) {
-				factors.add(number);
-			}
+		}
+		if(number >1) {
+			factors.add(number);
 		}
 		return factors;
 	}
