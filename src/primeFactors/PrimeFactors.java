e6044b21b7bc2077e13a999baa6bbc596af5b984
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 23:25:37 +0900

    Red-primefactor_of_1

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -1,5 +1,11 @@
 package primeFactors;
 
+import java.util.List;
+
 public class PrimeFactors {
+
+	public List<Integer> of(int i) {
+		return null;
+	}
 
 }
--- test/primeFactors/PrimeFactorsTest.java
+++ test/primeFactors/PrimeFactorsTest.java
@@ -1,14 +1,20 @@
 package primeFactors;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class PrimeFactorsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void primefactor_of_1() {
+		PrimeFactors primeFactors = new PrimeFactors();
+		List<Integer> factors = primeFactors.of(1);
+		
+		assertEquals(Arrays.asList(), factors);
 	}
 
 }
