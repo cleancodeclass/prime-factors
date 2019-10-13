6215a4d1d446279ea60d1363e30f11bc21ecdfb1
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 23:42:48 +0900

    Refactor- convert outer 'while' loop statement to 'for' statement for readability

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -7,13 +7,13 @@ public class PrimeFactors {
 
 	public List<Integer> of(int number) {
 		List<Integer> factors = new ArrayList<Integer>();
-		int divisor = 2;
-		while(number>1) {
+		
+		for(int divisor = 2;number>1;divisor++) {
 			while(number % divisor == 0) {
 				factors.add(divisor);
 				number /= divisor;
 			}
-			divisor++;
+			
 		}
 		return factors;
 	}
