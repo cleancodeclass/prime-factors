73ae844ab05c485c3607a43eaff4e1fc2489a85d
doni lee <doni@LAPTOP-9Q54PU9H>
2019-10-13 23:31:49 +0900

    Green-primefactor_of_4

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -8,7 +8,13 @@ public class PrimeFactors {
 	public List<Integer> of(int number) {
 		List<Integer> factors = new ArrayList<Integer>();
 		if(number>1) {
-			factors.add(number);
+			if(number % 2 == 0) {
+				factors.add(2);
+				number /= 2;
+			}
+			if(number >1) {
+				factors.add(number);
+			}
 		}
 		return factors;
 	}
